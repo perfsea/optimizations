b6bbdfc3e463adb790a173ab520d9263a32d5188
Perf Sea <support@perfsea.com>
2024-12-04 20:16:23 -0800

    more threads

--- benchmarks/run.sh
+++ benchmarks/run.sh
@@ -13,7 +13,7 @@ sleep 5
 ssh -i "terraform_ec2_key" -o "StrictHostKeyChecking no" ec2-user@$server_ip 'bash' < $1
 ssh -i "terraform_ec2_key" -o "StrictHostKeyChecking no" ubuntu@$client_ip 'bash' < setup_memtier.sh
 ssh -i "terraform_ec2_key" -o "StrictHostKeyChecking no" ec2-user@$server_ip 'bash' < profile.sh
-ssh -i "terraform_ec2_key" -o "StrictHostKeyChecking no" ubuntu@$client_ip "memtier_benchmark -s $server_ip_private --key-maximum=10000 $2"
+ssh -i "terraform_ec2_key" -o "StrictHostKeyChecking no" ubuntu@$client_ip "memtier_benchmark -s $server_ip_private --key-maximum=10000 --threads 64 $2"
 ssh -i "terraform_ec2_key" -o "StrictHostKeyChecking no" ec2-user@$server_ip 'bash' < process_profile.sh
 scp -i "terraform_ec2_key" -o "StrictHostKeyChecking no" ec2-user@$server_ip:profile* .
 
