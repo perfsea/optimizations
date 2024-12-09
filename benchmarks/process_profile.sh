2ab9930328bd526b974e26ea72d52cf0ef51a255
Perf Sea <support@perfsea.com>
2024-12-08 20:06:38 -0800

    cleaner process

--- benchmarks/process_profile.sh
+++ benchmarks/process_profile.sh
@@ -1,7 +1,4 @@
 sudo pkill -SIGINT perf
 sudo pkill iostat
 pidwait perf
-sudo perf script > out.perf &
-pidwait perf
-git clone https://github.com/brendangregg/FlameGraph.git
-./FlameGraph/stackcollapse-perf.pl out.perf > profile.col
+sudo perf script report stackcollapse > profile.col
